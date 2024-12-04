7cabed1f71567615710eb53e70c920231ab8af90
Alexander <mightee.cactus@gmail.com>
2024-12-04 23:10:56 +0300

    решение 2.1.2

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,13 +2,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
 @Configuration
 public class AppConfig {
- 
+
     @Bean(name="helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Bean(name="cat")
+    @Scope(value = SCOPE_PROTOTYPE)
+    public Cat getCat() {
+        Cat cat = new Cat();
+        return cat;
     }
 }
