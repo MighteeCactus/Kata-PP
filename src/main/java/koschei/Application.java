61a2be22e848f02c91dca7817e93859ea95508c2
Alexander <mightee.cactus@gmail.com>
2024-12-05 00:23:58 +0300

    шпаргалка

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -12,4 +12,11 @@ public class Application {
                 applicationContext.getBean(KoscheiTheDeathless.class);
         System.out.println(koscheiTheDeathless.getRulesByDeth());
     }
+
+    // шпаргалка
+    // injection через:
+    // 1. через метод с @Autowired в @Component классе, подтягивает аргументы как зависимости
+    // 2. проперт + @Autowire в @Component классе, сама зависимость
+    //      объявлена как метод с @Bean в @Configuration классе, в которую (в bean) так же подтягивает аргументы как зависимости
+    // 3. через конструктор + @Autowire (подтягивает ппараметры как зависимости
 }
